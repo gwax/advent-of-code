17bbc98700f7800cca367b4f5f010e4592265543
George Leslie-Waksman <waksman@gmail.com>
2017-12-14 01:07:02 -0800

    day10

---
+++ src/main/kotlin/com/gwax/aoc2017/day10/KnotHash.kt
@@ -0,0 +1,71 @@
+package com.gwax.aoc2017.day10
+
+data class StepResult(val position: Int, val skip: Int)
+
+class DataCircle(val size: Int) {
+    constructor(elements: List<Int>) : this(elements.size) {
+        _elements = elements.toMutableList()
+    }
+    private var _elements = (0 until size).toMutableList()
+    val elements: List<Int>
+        get() = _elements.toList()
+
+    override fun equals(other: Any?): Boolean {
+        if (other is DataCircle) return this.elements == other.elements
+        return super.equals(other)
+    }
+
+    override fun toString(): String = _elements.joinToString(prefix = "(~", separator = "-", postfix = "~)")
+
+    fun rotate(distance: Int) {
+        val rotation = if (distance < 0) size + distance else distance
+        _elements = (_elements.drop(rotation) + _elements.dropLast(size - rotation)).toMutableList()
+    }
+    fun tieKnot(position: Int, length: Int) {
+        if (length > size) return
+        rotate(position)
+        _elements = (_elements.subList(0, length).reversed() + _elements.drop(length)).toMutableList()
+        rotate(-position)
+    }
+
+    fun hashStep(position: Int, length: Int, skip: Int): StepResult {
+        if (length > size) return StepResult(position, skip)
+        tieKnot(position, length)
+        return StepResult((position + length + skip) % size, skip + 1)
+    }
+
+    fun knotHash(lengths: List<Int>, times: Int = 1): StepResult {
+        var step = StepResult(0, 0)
+        repeat(times) {
+            lengths.forEach {
+                step = hashStep(step.position, it, step.skip)
+            }
+        }
+        return step
+    }
+}
+
+fun denseHash(circle: DataCircle): String =
+        circle.elements.chunked(16, {
+            it.reduce { a, b -> a xor b }
+                    .toString(16)
+                    .padStart(2, '0') })
+                .joinToString("")
+
+fun hashLengths(input: String): List<Int> =
+        input.map { it.toInt() }.toList() + listOf(17, 31, 73, 47, 23)
+
+
+fun main(args: Array<String>) {
+    val lengths1 = input.split(",").map { it.toInt() }
+    val circle1 = DataCircle(256)
+    circle1.knotHash(lengths1)
+    println(circle1.elements[0] * circle1.elements[1])
+
+    val lengths2 = hashLengths(input)
+    val circle2 = DataCircle(256)
+    circle2.knotHash(lengths2, 64)
+    println(denseHash(circle2))
+}
+
+val input = "147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70"
---
+++ src/test/kotlin/com/gwax/aoc2017/day10/KnotHashTest.kt
@@ -0,0 +1,60 @@
+package com.gwax.aoc2017.day10
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DataCircleTest {
+    @Test
+    fun basicFunctionsTest() {
+        assertEquals(DataCircle(2), DataCircle(listOf(0, 1)))
+        assertEquals("(~0-1~)", DataCircle(2).toString())
+        assertEquals(listOf(0, 1), DataCircle(2).elements)
+    }
+
+    @Test
+    fun rotationTest() {
+        val circle = DataCircle(5)
+        assertEquals(listOf(0, 1, 2, 3, 4), circle.elements)
+        circle.rotate(2)
+        assertEquals(listOf(2, 3, 4, 0, 1), circle.elements)
+        circle.rotate(-3)
+        assertEquals(listOf(4, 0, 1, 2, 3), circle.elements)
+    }
+
+    @Test
+    fun tieKnotTest() {
+        val circle = DataCircle(5)
+        assertEquals(listOf(0, 1, 2, 3, 4), circle.elements)
+        circle.tieKnot(1, 2)
+        assertEquals(listOf(0, 2, 1, 3, 4), circle.elements)
+        circle.tieKnot(4, 3)
+        assertEquals(listOf(0, 4, 1, 3, 2), circle.elements)
+    }
+
+    @Test
+    fun hashStepTest() {
+        val circle = DataCircle(5)
+        val result = circle.hashStep(0, 3, 0)
+        assertEquals(listOf(2, 1, 0, 3, 4), circle.elements)
+        assertEquals(StepResult(3, 1), result)
+    }
+
+    @Test
+    fun knotHashTest() {
+        val circle = DataCircle(5)
+        circle.knotHash(listOf(3, 4, 1, 5))
+        assertEquals(listOf(3, 4, 2, 1, 0), circle.elements)
+    }
+
+    @Test
+    fun asciiLengthsTest() {
+        assertEquals(listOf(49,44,50,44,51,17,31,73,47,23), hashLengths("1,2,3"))
+    }
+
+    @Test
+    fun denseHashTest() {
+        val circle = DataCircle(256)
+        circle.knotHash(hashLengths(""), times=64)
+        assertEquals("a2582a3a0e66e6e86e3812dcb672a272", denseHash(circle))
+    }
+}
