0cdcab547e0fa2e22aac9ef05534711a9c607969
George Leslie-Waksman <waksman@gmail.com>
2017-12-19 13:16:07 -0800

    day18 rewrite + part 2, not so much on the tests

--- src/main/kotlin/com/gwax/aoc2017/day18/Duet.kt
+++ src/main/kotlin/com/gwax/aoc2017/day18/Duet.kt
@@ -1,19 +1,11 @@
 package com.gwax.aoc2017.day18
 
 import com.gwax.aoc2017.day15.withTiming
-import java.math.BigInteger
 
-const val SND_KEY = '\u0007'  // Bell control character
 typealias Memory = MutableMap<Char, Long>
-typealias Program = List<Operation>
 
-class RecoverException(val snd: Long) : Exception() {
-    override val message: String?
-        get() = "Recovered $snd"
-}
-
-interface Value {
-    fun retrieve(memory: Memory): Long
+sealed class Value {
+    abstract fun retrieve(memory: Memory): Long
 }
 fun value(input: String): Value =
         try {
@@ -21,21 +13,24 @@ fun value(input: String): Value =
         } catch (_: NumberFormatException) {
             Register(input.first())
         }
-
-data class Register(val name: Char) : Value {
+data class Register(val name: Char): Value() {
     override fun retrieve(memory: Memory) = memory.getOrDefault(name, 0)
     fun put(memory: Memory, value: Long) {
         memory[name] = value
     }
 }
-
-data class Constant(val value: Long) : Value {
+data class Constant(val value: Long) : Value() {
     override fun retrieve(memory: Memory) = value
 }
 
-interface Operation {
-    fun perform(memory: Memory): Int
-}
+sealed class Operation
+data class Snd(val x: Value) : Operation()
+data class Set(val x: Register, val y: Value) : Operation()
+data class Add(val x: Register, val y: Value) : Operation()
+data class Mul(val x: Register, val y: Value) : Operation()
+data class Mod(val x: Register, val y: Value) : Operation()
+data class Rcv(val x: Register) : Operation()
+data class Jgz(val x: Value, val y: Value) : Operation()
 fun operation(args: List<String>): Operation = when(args[0]) {
     "snd" -> Snd(value(args[1]))
     "set" -> Set(value(args[1]) as Register, value(args[2]))
@@ -47,79 +42,120 @@ fun operation(args: List<String>): Operation = when(args[0]) {
     else -> throw IllegalArgumentException(args.joinToString())
 }
 
-data class Snd(val x: Value) : Operation {
-    override fun perform(memory: Memory): Int {
-        memory[SND_KEY] = x.retrieve(memory)
-        return 1
+enum class Status { Active, Blocked, Terminated }
+
+open class Processor(val instructions: List<Operation>, var exPointer: Int, val registers: Memory, val readBuffer: MutableList<Long>, val writeBuffer: MutableList<Long>) {
+    var overrideStatus: Status? = null
+    fun status(): Status {
+        val currentOverride = overrideStatus
+        return when {
+            currentOverride != null -> currentOverride
+            exPointer < 0 || exPointer >= instructions.size -> Status.Terminated
+            instructions[exPointer] is Rcv && readBuffer.isEmpty() -> Status.Blocked
+            else -> Status.Active
+        }
+    }
+    open fun receive(op: Rcv) {
+        println(readBuffer.last())
+        overrideStatus = Status.Terminated
+    }
+    open fun send(op: Snd) {
+        writeBuffer.add(op.x.retrieve(registers))
+    }
+    fun execute() {
+        if (status() != Status.Active) return
+        val op = instructions[exPointer]
+        when(op) {
+            is Snd -> {
+                send(op)
+                exPointer++
+            }
+            is Set -> {
+                op.x.put(registers, op.y.retrieve(registers))
+                exPointer++
+            }
+            is Add -> {
+                op.x.put(registers, op.x.retrieve(registers) + op.y.retrieve(registers))
+                exPointer++
+            }
+            is Mul -> {
+                op.x.put(registers, op.x.retrieve(registers) * op.y.retrieve(registers))
+                exPointer++
+            }
+            is Mod -> {
+                op.x.put(registers, op.x.retrieve(registers) % op.y.retrieve(registers))
+                exPointer++
+            }
+            is Rcv -> {
+                receive(op)
+                exPointer++
+            }
+            is Jgz -> {
+                if (op.x.retrieve(registers) > 0)
+                    exPointer += op.y.retrieve(registers).toInt()
+                else
+                    exPointer++
+            }
+        }
     }
 }
 
-data class Set(val x: Register, val y: Value) : Operation {
-    override fun perform(memory: Memory): Int {
-        x.put(memory, y.retrieve(memory))
-        return 1
+class DuetProcessor(instructions: List<Operation>, exPointer: Int, registers: Memory, readBuffer: MutableList<Long>, writeBuffer: MutableList<Long>) :
+        Processor(instructions, exPointer, registers, readBuffer, writeBuffer) {
+    val name = registers['p']
+    var sendCount = 0
+    override fun receive(op: Rcv) {
+        val newValue = readBuffer.removeAt(0)
+//        println("$name:Receive ${op.x}:$newValue")
+        op.x.put(registers, newValue)
+    }
+
+    override fun send(op: Snd) {
+//        println("$name:Send ${op.x}${op.x.retrieve(registers)}")
+        super.send(op)
+        sendCount++
     }
 }
 
-data class Add(val x: Register, val y: Value) : Operation {
-    override fun perform(memory: Memory): Int {
-        x.put(memory, x.retrieve(memory) + y.retrieve(memory))
-        return 1
-    }
-}
-
-data class Mul(val x: Register, val y: Value) : Operation {
-    override fun perform(memory: Memory): Int {
-        x.put(memory, x.retrieve(memory) * y.retrieve(memory))
-        return 1
-    }
-}
-
-data class Mod(val x: Register, val y: Value) : Operation {
-    override fun perform(memory: Memory): Int {
-        x.put(memory, x.retrieve(memory) % y.retrieve(memory))
-        return 1
-    }
-}
-
-data class Rcv(val x: Register) : Operation {
-    override fun perform(memory: Memory): Int {
-        if (x.retrieve(memory) == 0L) return 1
-        throw RecoverException(memory.getOrDefault(SND_KEY, 0L))
-    }
-}
-
-data class Jgz(val x: Value, val y: Value) : Operation {
-    override fun perform(memory: Memory): Int {
-        return if (x.retrieve(memory) > 0 )
-            y.retrieve(memory).toInt()
-        else
-            1
-    }
-}
-
-fun parseProgram(input: String): Program =
+fun parseProgram(input: String): List<Operation> =
         input.lines()
             .map { it.split(" ") }
             .map(::operation)
 
-fun execute(program: Program) {
-    var pointer = 0
-    val memory = mutableMapOf<Char, Long>()
-    while (pointer >= 0 && pointer < program.size) {
-        val operation = program[pointer]
-        val jmp = operation.perform(memory)
-        pointer += jmp
-    }
+fun processorLoop(instructions: List<Operation>) {
+    val buffer = mutableListOf<Long>()
+    val processor = Processor(instructions, 0, mutableMapOf(), buffer, buffer)
+    do {
+        processor.execute()
+    } while (processor.status() == Status.Active)
+}
+
+fun processorDualLoop(instructions: List<Operation>) {
+    val queue0 = mutableListOf<Long>()
+    val queue1 = mutableListOf<Long>()
+    val processor0 = DuetProcessor(
+            instructions,
+            0,
+            mutableMapOf('p' to 0L),
+            queue0,
+            queue1)
+    val processor1 = DuetProcessor(
+            instructions,
+            0,
+            mutableMapOf('p' to 1L),
+            queue1,
+            queue0)
+    do {
+        processor0.execute()
+        processor1.execute()
+    } while (processor0.status() == Status.Active || processor1.status() == Status.Active)
+    println(processor1.sendCount)
 }
 
 fun main(args: Array<String>) = withTiming {
     val program = parseProgram(input)
-    try {
-        execute(program)
-    } catch (recover: RecoverException) {
-        println(recover.snd)
-    }
+    processorLoop(program)
+    processorDualLoop(program)
 }
 
 
--- src/test/kotlin/com/gwax/aoc2017/day18/DuetTest.kt
+++ src/test/kotlin/com/gwax/aoc2017/day18/DuetTest.kt
@@ -2,7 +2,6 @@ package com.gwax.aoc2017.day18
 
 import org.junit.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
 
 class DuetTest {
     val memory = mutableMapOf<Char, Long>()
@@ -15,74 +14,6 @@ class DuetTest {
         assertEquals(5, const.retrieve(memory))
         reg.put(memory, 12)
         assertEquals(12, reg.retrieve(memory))
-    }
-
-    @Test
-    fun sndTest() {
-        memory['a'] = 1
-        val jmp = Snd(Register('a')).perform(memory)
-        assertEquals(1, memory[SND_KEY])
-        assertEquals(1, jmp)
-    }
-
-    @Test
-    fun setTest() {
-        memory['a'] = 3
-        val jmp1 = Set(Register('b'), Register('a')).perform(memory)
-        val jmp2 = Set(Register('c'), Constant(2)).perform(memory)
-        assertEquals(3, memory['b'])
-        assertEquals(2, memory['c'])
-        assertEquals(1, jmp1)
-        assertEquals(1, jmp2)
-    }
-
-    @Test
-    fun addTest() {
-        memory['d'] = 4
-        val jmp = Add(Register('d'), Constant(1)).perform(memory)
-        assertEquals(5, memory['d'])
-        assertEquals(1, jmp)
-    }
-
-    @Test
-    fun mulTest() {
-        memory['e'] = 3
-        val jmp = Mul(Register('e'), Constant(2)).perform(memory)
-        assertEquals(6, memory['e'])
-        assertEquals(1, jmp)
-    }
-
-    @Test
-    fun modTest() {
-        memory['f'] = 17
-        val jmp = Mod(Register('f'), Constant(9)).perform(memory)
-        assertEquals(8, memory['f'])
-        assertEquals(1, jmp)
-    }
-
-    @Test
-    fun rcvTest() {
-        memory[SND_KEY] = 3
-        memory['a'] = 0
-        val jmp = Rcv(Register('a')).perform(memory)
-        assertEquals(1, jmp)
-        memory['a'] = 1
-        val recovered = assertFailsWith(RecoverException::class, {
-            Rcv(Register('a')).perform(memory)
-        })
-        assertEquals(3, recovered.snd)
-    }
-
-    @Test
-    fun jgzTest() {
-        memory['a'] = 0
-        assertEquals(
-                1,
-                Jgz(Register('a'), Constant(5)).perform(memory))
-        memory['a'] = 1
-        assertEquals(
-                5,
-                Jgz(Register('a'), Constant(5)).perform(memory))
     }
 
     @Test
@@ -115,22 +46,16 @@ class DuetTest {
     }
 
     @Test
-    fun executeTest() {
-        val program = listOf(
-                Set(Register('a'), Constant(1)),
-                Add(Register('a'), Constant(2)),
-                Mul(Register('a'), Register('a')),
-                Mod(Register('a'), Constant(5)),
-                Snd(Register('a')),
-                Set(Register('a'), Constant(0)),
-                Rcv(Register('a')),
-                Jgz(Register('a'), Constant(-1)),
-                Set(Register('a'), Constant(1)),
-                Jgz(Register('a'), Constant(-2))
-        )
-        val recover = assertFailsWith(RecoverException::class, {
-            execute(program)
-        })
-        assertEquals(4, recover.snd)
+    fun processorDualLoopTest() {
+        val input = """
+            snd 1
+            snd 2
+            snd p
+            rcv a
+            rcv b
+            rcv c
+            rcv d
+            """.trimIndent()
+            processorDualLoop(parseProgram(input))
     }
 }
